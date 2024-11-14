978a0d0b994e919f5610409ba1b22d390aa12d39
David Jomain <david@bity.com>
2024-11-15 00:00:53 +0100

    feat: configure tailwindcss, fonts, assets

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,26 @@
-import type { Config } from "tailwindcss";
+/** @format */
+
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        white: '#FFFFFF',
+        black: '#000000',
+        primary: '#FBECE7',
+        secondary: '#563828',
+        accent: '#FF0000',
+      },
+      fontFamily: {
+        inter: 'var(--font-inter)',
       },
     },
   },
